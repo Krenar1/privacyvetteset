67de01c90a4bbe48e470ffa047031c63961b318e
Krenar1 <111074309+Krenar1@users.noreply.github.com>
2025-04-20 16:53:52 +0200

    Update apiLink.ts

--- apiLink.ts
+++ apiLink.ts
@@ -2,7 +2,6 @@ const ApiLink: { url: string } = {
     url: process.env.NODE_ENV === 'development' 
       ? "http://127.0.0.1:8000/" 
       : "https://api.privacyvet.com/",
-        'https://www.proud-marketing.net/',
 };
 
 export default ApiLink;
